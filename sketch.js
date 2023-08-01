28bdf2dd81b6a6f49ad7bf9a1157020537d51b7a
ProfMara <94469709+ProfMara@users.noreply.github.com>
2023-08-01 16:52:40 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var canvas;
+var cenario, form;
+var database;
+var gameState = 0;
+var carimg1, carimg2;
+var pista, player;
+var car1, car2;
+var cars = [];
+var playerCount = 0;
+function preload() {
+}
+
+function setup() {
+
+
+}
+
+function draw() {
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
